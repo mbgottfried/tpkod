e34a071bdf5d71b0deae0bc833825979d500de98
shagmbg <michael.b.gottfried@gmail.com>
2013-11-08 11:34:06 -0500

    starting to style

--- config/initializers/koudoku.rb
+++ config/initializers/koudoku.rb
@@ -1,7 +1,7 @@
 Koudoku.setup do |config|
   config.webhooks_api_key = "590b38ec-8a74-4a27-9e3c-e1ac77423954"
   config.subscriptions_owned_by = :user
-  config.stripe_publishable_key = ENV['STRIPE_PUBLISHABLE_KEY']
-  config.stripe_secret_key = ENV['STRIPE_SECRET_KEY']
+  config.stripe_publishable_key = "pk_test_ybms4ZSc8kP1FnekvyTbp7Wn"
+  config.stripe_secret_key = "sk_test_cgajMxpMgtzyEy4fdPKC9zzd"
   # config.free_trial_length = 30
 end
---
+++ config/initializers/stripe.rb
@@ -0,0 +1,2 @@
+Stripe.api_key = "sk_test_cgajMxpMgtzyEy4fdPKC9zzd"
+STRIPE_PUBLISHABLE_KEY = "pk_test_ybms4ZSc8kP1FnekvyTbp7Wn"
